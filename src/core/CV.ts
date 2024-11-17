d8ced9922649942b55881a00eef6f68a32e846bf
Andrew Liu <48308664+lzyzlzy@users.noreply.github.com>
2024-11-17 23:52:20 +0800

    Create CV.ts

---
+++ src/core/CV.ts
@@ -0,0 +1,27 @@
+export interface Cv {
+    name: string;
+    birthday?: Date;
+    phoneNumber: string;
+    jobTitle: string;
+    email: string;
+    website?: string;
+    github?: string;
+    twitter?: string;
+    linkedin?: string;
+    stackOverFlow?: string;
+    experiences: CvEvent[];
+    skills: string[];
+    educations: CvEvent[];
+    languages?: LanguagesSkill[];
+    projects: CvEvent[];
+}
+export interface CvEvent {
+    title?: string,
+    content: string,
+    from?: Date | string,
+    to?: Date | string
+}
+export interface LanguagesSkill {
+    name: string;
+    level: string;
+}
