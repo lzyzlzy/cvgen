5372a3ac84d26e2c8e8ab9eef747486ee1ff8395
liuzhenyu <liuzhenyu@ssit.cc>
2024-11-19 17:13:23 +0800

    update editor

--- src/core/CV.ts
+++ src/core/CV.ts
@@ -1,26 +1,28 @@
 export interface Cv {
-    name: string;
-    birthday?: Date;
-    phoneNumber: string;
-    jobTitle: string;
-    email: string;
-    website?: string;
-    github?: string;
-    twitter?: string;
-    linkedin?: string;
-    experiences: CvEvent[];
-    skills: string[];
-    educations: CvEvent[];
-    languages?: LanguagesSkill[];
-    projects: CvEvent[];
+  name: string;
+  birthday?: Date;
+  phoneNumber: string;
+  jobTitle: string;
+  email: string;
+  website?: string;
+  github?: string;
+  twitter?: string;
+  linkedin?: string;
+  experiences: CvEvent[];
+  skills: string[];
+  educations: CvEvent[];
+  languages?: LanguagesSkill[];
+  projects: CvEvent[];
 }
 export interface CvEvent {
-    title?: string,
-    content: string,
-    from?: Date | string,
-    to?: Date | string
+  title?: string;
+  subTitle?: string;
+  content: string;
+  from?: Date | undefined;
+  to?: Date | undefined;
+  url?: string;
 }
 export interface LanguagesSkill {
-    name: string;
-    level: string;
+  name: string;
+  level: string;
 }
