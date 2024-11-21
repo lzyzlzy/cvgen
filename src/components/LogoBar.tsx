5c6e568b4c316362cddfb22c9a54a4ed5b01847b
liuzhenyu <liuzhenyu@ssit.cc>
2024-11-21 17:03:54 +0800

    add fr jp es ru support,add github link

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -1,10 +1,28 @@
+import { Github } from "lucide-react";
 import { LanguageSelect } from "./LanguageSelect";
+import { Button } from "./ui/button";
 
 export function LogoBar() {
   return (
     <div className="border-b px-6 py-4 sticky top-0 w-full bg-white flex flex-row justify-between items-center">
       <div className="text-3xl font-bold">CVGEN</div>
-      <LanguageSelect />
+      <div className="space-x-2">
+        <LanguageSelect />
+        <GithubButton />
+      </div>
     </div>
+  );
+}
+
+function GithubButton() {
+  return (
+    <Button
+      size="icon"
+      variant="outline"
+      className="hover:border-none "
+      onClick={() => window.open("https://github.com/lzyzlzy/cvgen", "_blank")}
+    >
+      <Github className="text-black" />
+    </Button>
   );
 }
